68df801b87af74755988cb53c49a5a534063f449
ongonginging <ongonginging@gmail.com>
2015-10-18 21:21:19 +0800

    1

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm -rf ./a.out
-g++ main.cc test.cc base.cc base.h class.h func.h -L/usr/local/lib/gtest -lgtest -I./ -I/usr/local/include -g -O0 -Werror
+g++ main.cc test.cc base.cc base.h class.h func.h -L/usr/local/lib/gtest -L/usr/lib/x86_64-linux-gnu -lgtest -lpthread -I./ -I/usr/include -I/usr/local/include  -g -O0 -Werror
